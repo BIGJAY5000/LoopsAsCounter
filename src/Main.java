149522d92927e6ad9855701251ad0276ea45c7a9
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-10-30 09:38:38 -0400

    cdcc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i <= 30; i++) {
+            System.out.printf("%-4d", i);
+        }
+        System.out.println("");
+        for (int i = 30; i >= 0; i--) {
+            System.out.printf("%-4d", i);
+        }
+        System.out.println("");
+        for (int i = 0; i <= 18; i+=3) {
+            System.out.printf("%-4d", i);
+        }
+        System.out.println("");
+        for (int i = 10; i >= 0; i-=2) {
+            System.out.printf("%-4d", i);
+        }
     }
 }
